41628f8443fc08891128a64e6f9f533fe750d7c1
lbertoncello <lucas_b_andrade@hotmail.com>
2018-10-24 17:47:49 -0300

    Add files via upload

--- create_subsets.py
+++ create_subsets.py
@@ -1,7 +1,46 @@
 import numpy as np
+import sys
+import os
 from sklearn.model_selection import StratifiedKFold
 
-def create_subset(dataset_file_name, classes_file_name):
+n_splits = 4
+
+def create_subset(dataset_file_name, classes_file_name, subsets_dirname):
+	dataset, classes = read_files(dataset_file_name, classes_file_name)
+
+	np.set_printoptions(precision=2)
+
+	skf = StratifiedKFold(n_splits=n_splits, shuffle=True)
+
+	accuracy_avg = 0
+	precision_avg = 0
+	recall_avg = 0
+	duration_avg = 0
+	i = 0
+
+	for train_index, test_index in skf.split(dataset, classes):
+		dirname = "%s/%s" % (subsets_dirname, i)
+		os.mkdir(dirname)
+
+		with open('%s/treino.txt' % dirname, 'w') as f:
+			for index in train_index:
+				f.write("%s\n" % str(" ".join(map(str, dataset[index].astype(np.float)))))
+			
+		with open('%s/classes.txt' % dirname, 'w') as f:
+			for index in train_index:
+				f.write("%s\n" % str(" ".join(map(str, classes[index]))))
+				
+		with open('%s/entrada.txt' % dirname, 'w') as f:
+			for index in test_index:
+				f.write("%s\n" % str(" ".join(map(str, dataset[index].astype(np.float)))))
+		
+		with open('%s/classes_corretas.txt' % dirname, 'w') as f:
+			for index in test_index:
+				f.write("%s\n" % str(" ".join(map(str, classes[index]))))
+
+		i += 1
+
+def read_files(dataset_file_name, classes_file_name):
 	dataset_file = open(dataset_file_name, 'r')
 	lines = dataset_file.readlines()
 
@@ -26,3 +65,13 @@ def create_subset(dataset_file_name, classes_file_name):
 		classes.append(vec)
 	
 	return (dataset, classes)
+
+def main():
+	dataset_file_name = sys.argv[1]
+	classes_file_name = sys.argv[2]
+	subsets_dirname = sys.argv[3]
+
+	create_subset(dataset_file_name, classes_file_name, subsets_dirname)
+
+if __name__ == "__main__":
+    main()
